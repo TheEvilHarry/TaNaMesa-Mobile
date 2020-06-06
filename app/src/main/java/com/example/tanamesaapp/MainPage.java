1dd5cb593074fa156ecdc12501dc434976f47a14
henrique <hsbarboza@inf.ufrgs.br>
2020-06-06 01:38:03 -0300

    Adding firebase properties

--- app/src/main/java/com/example/tanamesaapp/MainPage.java
+++ app/src/main/java/com/example/tanamesaapp/MainPage.java
@@ -1,30 +1,23 @@
 package com.example.tanamesaapp;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
 
-    private Button button;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_page);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        button = (Button) findViewById(R.id.button);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
@@ -32,10 +25,4 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
-
-    public void openMainPage(View v){
-        Intent intent = new Intent(this,MainPage.class);
-        startActivity(intent);
-    }
-
 }
