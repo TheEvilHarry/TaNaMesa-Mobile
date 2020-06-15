7bcdc9f696d9a66e3efc212b33fd3d29cd6f76c9
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2020-06-14 23:05:28 -0300

    wip

---
+++ app/src/main/java/com/example/tanamesaapp/models/Categories.java
@@ -0,0 +1,93 @@
+/*-----------------------------------------------------------------------------
+ - Developed by Haerul Muttaqin                                               -
+ - Last modified 3/17/19 5:24 AM                                              -
+ - Subscribe : https://www.youtube.com/haerulmuttaqin                         -
+ - Copyright (c) 2019. All rights reserved                                    -
+ -----------------------------------------------------------------------------*/
+package com.example.tanamesaapp.models;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Categories implements Serializable {
+
+    @SerializedName("categories")
+    @Expose
+    private List<Category> categories;
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public static class Category implements Serializable {
+
+        @SerializedName("idCategory")
+        @Expose
+        private String idCategory;
+        @SerializedName("strCategory")
+        @Expose
+        private String strCategory;
+        @SerializedName("strCategoryThumb")
+        @Expose
+        private String strCategoryThumb;
+        @SerializedName("strCategoryDescription")
+        @Expose
+        private String strCategoryDescription;
+
+        public String getIdCategory() {
+            return idCategory;
+        }
+
+        public void setIdCategory(String idCategory) {
+            this.idCategory = idCategory;
+        }
+
+        public String getStrCategory() {
+            return strCategory;
+        }
+
+        public void setStrCategory(String strCategory) {
+            this.strCategory = strCategory;
+        }
+
+        public String getStrCategoryThumb() {
+            return strCategoryThumb;
+        }
+
+        public void setStrCategoryThumb(String strCategoryThumb) {
+            this.strCategoryThumb = strCategoryThumb;
+        }
+
+        public String getStrCategoryDescription() {
+            return strCategoryDescription;
+        }
+
+        public void setStrCategoryDescription(String strCategoryDescription) {
+            this.strCategoryDescription = strCategoryDescription;
+        }
+
+        public Category(String idCategory, String strCategory, String strCategoryThumb, String strCategoryDescription) {
+            this.idCategory = idCategory;
+            this.strCategory = strCategory;
+            this.strCategoryThumb = strCategoryThumb;
+            this.strCategoryDescription = strCategoryDescription;
+        }
+
+        @Override
+        public String toString() {
+            return "\n idCategory: " + idCategory
+                    + "\n" + "strCategory: " + strCategory
+                    + "\n" + "strCategoryThumb: " + strCategoryThumb
+                    + "\n" + "strCategoryDescription: " + strCategoryDescription;
+        }
+    }
+
+}
