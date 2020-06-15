cc5e1a74e90d0675f96bfff0661b852dfd2d0928
henrique <hsbarboza@inf.ufrgs.br>
2020-06-15 02:29:36 -0300

    Changing order confirmation page from Fragment -> Activity

--- app/src/main/java/com/example/tanamesaapp/OrderConfirmation.java
+++ app/src/main/java/com/example/tanamesaapp/OrderConfirmation.java
@@ -9,8 +9,10 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -20,9 +22,10 @@ import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class OrderConfirmation extends AppCompatActivity {
+public class OrderConfirmation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     private ListView listView;
+    private BottomNavigationView menu;
 
     String titles[] = {"Pizza 4 Queijos", "Guaraná"};
     String descriptions[] = {"49,50", "5.50"};
@@ -39,15 +42,33 @@ public class OrderConfirmation extends AppCompatActivity {
         listView.setAdapter(adapter);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_notifications: {
+                Intent intent = new Intent(this,CloseAccount.class);
+                startActivity(intent);
+            }
+            case R.id.navigation_dashboard: {
+                break;
+            }
+            case R.id.navigation_home: {
+                break;
+            }
+        }
+        return true;
+    }
+
 
     private void setupUI(){
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment1);
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        menu = (BottomNavigationView) findViewById(R.id.nav_view);
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment1);
+//        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
     }
 
 
