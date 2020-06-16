fc266066caf60550f2d91748414e03f5332fa8c5
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2020-06-15 22:48:58 -0300

    Implement confirmation Dialog

--- app/src/main/java/com/example/tanamesaapp/ui/detail/DetailActivity.java
+++ app/src/main/java/com/example/tanamesaapp/ui/detail/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.tanamesaapp.ui.detail;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
@@ -18,12 +19,15 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tanamesaapp.R;
 import com.example.tanamesaapp.Utils;
 import com.example.tanamesaapp.models.Meals;
 import com.squareup.picasso.Picasso;
 import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.app.AlertDialog;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -146,72 +150,88 @@ public class DetailActivity extends AppCompatActivity implements DetailView {
 
         //===
 
-        if (!meal.getStrIngredient1().isEmpty()) {
+        if (meal.getStrIngredient1() != null && !meal.getStrIngredient1().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient1());
         }
-        if (!meal.getStrIngredient2().isEmpty()) {
+        if (meal.getStrIngredient2() != null && !meal.getStrIngredient2().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient2());
         }
-        if (!meal.getStrIngredient3().isEmpty()) {
+        if (meal.getStrIngredient3() != null && !meal.getStrIngredient3().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient3());
         }
-        if (!meal.getStrIngredient4().isEmpty()) {
+        if (meal.getStrIngredient4() != null && !meal.getStrIngredient4().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient4());
         }
-        if (!meal.getStrIngredient5().isEmpty()) {
+        if (meal.getStrIngredient5() != null && !meal.getStrIngredient5().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient5());
         }
-        if (!meal.getStrIngredient6().isEmpty()) {
+        if (meal.getStrIngredient6() != null && !meal.getStrIngredient6().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient6());
         }
-        if (!meal.getStrIngredient7().isEmpty()) {
+        if (meal.getStrIngredient7() != null && !meal.getStrIngredient7().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient7());
         }
-        if (!meal.getStrIngredient8().isEmpty()) {
+        if (meal.getStrIngredient8() != null && !meal.getStrIngredient8().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient8());
         }
-        if (!meal.getStrIngredient9().isEmpty()) {
+        if (meal.getStrIngredient9() != null && !meal.getStrIngredient9().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient9());
         }
-        if (!meal.getStrIngredient10().isEmpty()) {
+        if (meal.getStrIngredient10() != null &&!meal.getStrIngredient10().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient10());
         }
-        if (!meal.getStrIngredient11().isEmpty()) {
+        if (meal.getStrIngredient11() != null &&!meal.getStrIngredient11().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient11());
         }
-        if (!meal.getStrIngredient12().isEmpty()) {
+        if (meal.getStrIngredient12() != null &&!meal.getStrIngredient12().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient12());
         }
-        if (!meal.getStrIngredient13().isEmpty()) {
+        if (meal.getStrIngredient13() != null &&!meal.getStrIngredient13().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient13());
         }
-        if (!meal.getStrIngredient14().isEmpty()) {
+        if (meal.getStrIngredient14() != null &&!meal.getStrIngredient14().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient14());
         }
-        if (!meal.getStrIngredient15().isEmpty()) {
+        if (meal.getStrIngredient15() != null &&!meal.getStrIngredient15().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient15());
         }
-        if (!meal.getStrIngredient16().isEmpty()) {
+        if (meal.getStrIngredient16() != null && !meal.getStrIngredient16().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient16());
         }
-        if (!meal.getStrIngredient17().isEmpty()) {
+        if (meal.getStrIngredient17() != null && !meal.getStrIngredient17().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient17());
         }
-        if (!meal.getStrIngredient18().isEmpty()) {
+        if (meal.getStrIngredient18() != null && !meal.getStrIngredient18().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient18());
         }
-        if (!meal.getStrIngredient19().isEmpty()) {
+        if (meal.getStrIngredient19() != null && !meal.getStrIngredient19().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient19());
         }
-        if (!meal.getStrIngredient20().isEmpty()) {
+        if (meal.getStrIngredient20() != null && !meal.getStrIngredient20().isEmpty()) {
             ingredients.append("\n \u2022 " + meal.getStrIngredient20());
         }
 
 
         youtube.setOnClickListener(v -> {
-            Intent intentYoutube = new Intent(Intent.ACTION_VIEW);
-            intentYoutube.setData(Uri.parse(meal.getStrYoutube()));
-            startActivity(intentYoutube);
+            // setup the alert builder
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Confirmar Pedido");
+            builder.setMessage("Deseja mesmo pedir um "+ getIntent().getStringExtra(HomeActivity.EXTRA_DETAIL) + " ?");
+
+            // add the buttons
+            builder.setPositiveButton("Confirmo", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getApplicationContext(), "Seu pedido foi encaminhado para o sistema! Obrigado", Toast.LENGTH_LONG)
+                            .show();
+                    finish();
+                }
+            });
+            builder.setNegativeButton("Cancelar", null);
+
+            // create and show the alert dialog
+            AlertDialog dialog = builder.create();
+            dialog.show();
         });
 
         source.setOnClickListener(v -> {
@@ -221,6 +241,7 @@ public class DetailActivity extends AppCompatActivity implements DetailView {
             Intent ARIntent = new Intent(this, HelloArActivity.class);
             startActivity(ARIntent);
         });
+
 
     }
 
--- app/src/main/java/com/example/tanamesaapp/ui/detail/DetailPresenter.java
+++ app/src/main/java/com/example/tanamesaapp/ui/detail/DetailPresenter.java
@@ -1,5 +1,7 @@
 package com.example.tanamesaapp.ui.detail;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.example.tanamesaapp.Utils;
@@ -8,6 +10,8 @@ import com.example.tanamesaapp.models.Meals;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
+import static com.google.android.gms.vision.L.TAG;
 
 public class DetailPresenter {
     private DetailView view;
@@ -24,6 +28,8 @@ public class DetailPresenter {
                 .enqueue(new Callback<Meals>() {
                     @Override
                     public void onResponse(@NonNull Call<Meals> call, @NonNull Response<Meals> response) {
+                        Log.w(TAG, "getMealByCategory: mealsCall");
+                        Log.w(TAG, "getMealByCategory: " + Utils.getApi().getMealByName(mealName).request().url().toString());
                         view.hideLoading();
                         if (response.isSuccessful() && response.body() != null) {
                             view.setMeal(response.body().getMeals().get(0));
